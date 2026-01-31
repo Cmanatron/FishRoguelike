4f93e332711c8f922c58e73c3a04d228b6fb422e
Lucas Parter <117957097+LucasCodingg@users.noreply.github.com>
2026-01-31 14:27:22 -0500

    variety to fish types

--- fish/fish_move.gd
+++ fish/fish_move.gd
@@ -1,22 +1,51 @@
 extends CharacterBody2D
 
-const speed = 30 
+var speed = 30 
 var dir: Vector2
 var dirOption = [Vector2.RIGHT, Vector2.LEFT, Vector2.DOWN, Vector2.UP]
+var typeFront = "SMALL"
+var health = 100
 
 var chase: bool
 
+var type = [
+	"SMALL", "MED", "LARGE"
+]
+
 func _ready():
 	chase = false
+	type.shuffle() #This is for testing! In the actual game I imagine it would start with
+	#specific types and then become randomized???
+	typeFront = type.front()
+	print(typeFront)
+	match typeFront:
+		"SMALL":
+			speed = 100
+			$Sprite2D.texture=ResourceLoader.load("res://fish/fish1.png")
+			health = 20
+		"MED": 
+			speed = 50
+			$Sprite2D.texture=ResourceLoader.load("res://fish/fish2.png")
+			health = 50
+		"LARGE":
+			speed = 20
+			$Sprite2D.texture=ResourceLoader.load("res://fish/fish3.png")
+			health = 100
 	
 func _process(delta):
 	move(delta)
+	#health -= 10
+	#print(health)
 	
 func move(delta):
+			
 	if !chase:
 		velocity += dir * speed * delta
-	move_and_slide()
+		move_and_slide()
 		
+	if health == 0: 
+		queue_free()
+	
 
 func _on_timer_timeout():
 	$Timer.wait_time = randf_range(1.0, 2.0)
