8dda7611ae37e674d617c54d9c606632a995f680
Clancy M <Cmanatron@gmail.com>
2026-02-01 13:48:35 -0500

    DiverSPrite
    Diver spirte and bullet

--- Player/bullet.gd
+++ Player/bullet.gd
@@ -12,5 +12,6 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 	if(body.is_in_group("Enemy")):
 		print("hit")
 		body.health -= damage
-	$".".queue_free()
+	if(!body.is_in_group("Player")):
+		$".".queue_free()
 	
